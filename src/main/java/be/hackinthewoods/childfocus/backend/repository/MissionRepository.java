cd1d2a26199090ae1cb96e21774ce51e905bdcd5
fdpro <dupire.francois+pro@gmail.com>
2019-08-02 15:03:07 +0200

    Important update, hotfix

--- src/main/java/be/hackinthewoods/childfocus/backend/repository/MissionRepository.java
+++ src/main/java/be/hackinthewoods/childfocus/backend/repository/MissionRepository.java
@@ -3,5 +3,4 @@ package be.hackinthewoods.childfocus.backend.repository;
 import be.hackinthewoods.childfocus.backend.entity.Mission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissionRepository extends JpaRepository<Mission, Long> {
-}
+public interface MissionRepository extends JpaRepository<Mission, Long> {}
--- src/main/java/be/hackinthewoods/childfocus/backend/repository/PosterRepository.java
+++ src/main/java/be/hackinthewoods/childfocus/backend/repository/PosterRepository.java
@@ -3,6 +3,4 @@ package be.hackinthewoods.childfocus.backend.repository;
 import be.hackinthewoods.childfocus.backend.entity.Poster;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PosterRepository extends JpaRepository<Poster, Long> {
-
-}
+public interface PosterRepository extends JpaRepository<Poster, Long> {}
